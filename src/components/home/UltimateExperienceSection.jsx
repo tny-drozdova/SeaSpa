ce189974cc2ac1e922bf0325e921835a1a5e18eb
Tanya Drozdova <tny.drozdova@gmail.com>
2026-04-18 19:17:21 +0000

    Visual edits

--- src/components/home/UltimateExperienceSection.jsx
+++ src/components/home/UltimateExperienceSection.jsx
@@ -58,7 +58,7 @@ export default function UltimateExperienceSection({ image }) {
             className="relative">
             
             <div className="aspect-[3/2] rounded-lg overflow-hidden">
-              <img src="https://media.base44.com/images/public/69d4989a834518931660bae7/f0e5df5d6_couple_in_hot_tub_boat_Stone_harbor_NJ.jpg"
+              <img src="https://media.base44.com/images/public/69d4989a834518931660bae7/5583e456a_Friends_in_Tub.jpg"
 
               alt="Premium hot tub boat with crystal clear bubbling water, teak wood deck, and plush towels overlooking New Jersey Bay at sunset" className="w-full h-full object-cover"
 
